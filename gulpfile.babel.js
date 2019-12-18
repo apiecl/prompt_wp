d27b1c3a500cee92329f2ba9c4bc69acafebc064
Pablo Selin Carrasco Armijo <pablo@apie.cl>
2019-12-18 13:00:42 -0500

    timeline less workflow

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,7 @@
 import { src, dest, watch, series, parallel } from 'gulp';
 import yargs from 'yargs';
 import sass from 'gulp-sass';
+import less from 'gulp-less';
 import cleanCss from 'gulp-clean-css';
 import gulpif from 'gulp-if';
 import postcss from 'gulp-postcss';
@@ -21,9 +22,17 @@ export const styles = () => {
     .pipe(server.stream());
 }
 
+export const timelineStyles = () => {
+  return src('less/TL.Timeline.less')
+    .pipe(less())
+    .pipe(dest('.'))
+    .pipe(server.stream());
+}
+
 export const watchForChanges = () => {
   watch('sass/**/*.scss', styles);
   watch('**/*.php', styles, reload);
+  watch('less/**/*.less', timelineStyles);
 }
 
 const server = browserSync.create();
@@ -40,6 +49,6 @@ export const reload = done => {
 	done();
 }
 
-export const dev = series( styles, serve, watchForChanges )
-export const build = series( styles )
+export const dev = series( styles, timelineStyles, serve, watchForChanges )
+export const build = series( styles, timelineStyles )
 export default dev;
